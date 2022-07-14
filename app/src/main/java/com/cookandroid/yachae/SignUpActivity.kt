979396de776590853b185267783acf6269006a7b
seungeun-Leee <leesuel0379@gmail.com>
2022-07-14 18:28:03 +0900

    feature-google 로그인
    Add a SHA-1 fingerprint 필요

--- app/src/main/java/com/cookandroid/yachae/SignUpActivity.kt
+++ app/src/main/java/com/cookandroid/yachae/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package com.cookandroid.yachae
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,34 +9,87 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class SignUpActivity {
     class MainActivity : AppCompatActivity() {
         private lateinit var auth: FirebaseAuth
+        private var googleSignInClient : GoogleSignInClient? = null
         private val TAG = "SignUpActivity"
+        var GOOGLE_LOGIN_CODE = 9001
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_signup)
+
             // Initialize Firebase Auth
             auth = Firebase.auth
-
             var signUpButton : Button
+            var googleSignUnButton : Button
 
             signUpButton = findViewById<Button>(R.id.signUpButton)
-
             signUpButton.setOnClickListener{
                 Log.d("test log", "개발자용 로그")
                 signUp()
             }
+
+            googleSignUnButton = findViewById<Button>(R.id.googleSignUnButton)
+            googleSignUnButton.setOnClickListener {
+                //First step
+                googleLogin()
+            }
+            var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            googleSignInClient = GoogleSignIn.getClient(this, gso)
         }
 
         public override fun onStart() {
             super.onStart()
             val currentUser = auth.currentUser
+        }
+
+        fun googleLogin() {
+            var signInIntent = googleSignInClient?.signInIntent
+            startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if(requestCode == GOOGLE_LOGIN_CODE) {
+                var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
+                // result가 성공했을 때 이 값을 firebase에 넘겨주기
+                if(result!!.isSuccess) {
+                    var account = result.signInAccount
+                    // Second step
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+        }
+
+        private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+            var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+            auth?.signInWithCredential(credential)
+                ?.addOnCompleteListener {
+                        task ->
+                    if(task.isSuccessful) {
+                        //Login, 아이디와 패스워드가 맞았을 때
+                        Toast.makeText(this,  "success", Toast.LENGTH_LONG).show()
+                        //Log.d("성공", "성공")
+                    } else {
+                        // Show the error message, 아이디와 패스워드가 틀렸을 때
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }
+                }
         }
 
         public fun signUp() {
